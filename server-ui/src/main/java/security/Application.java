ab68036408ed109fc80e17d8b5719db81646bb13
busygina83 <busygina83@mail.ru>
2021-09-21 00:41:44 +0300

    Lab7

---
+++ server-ui/src/main/java/security/Application.java
@@ -0,0 +1,13 @@
+package security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("config")
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
